5b9062877993f3cbb975384f65f7dc25cb39574b
AK <adelinekgen@gmail.com>
2024-04-05 04:07:50 +0800

    reserve

--- client/src/Reservation.jsx
+++ client/src/Reservation.jsx
@@ -16,18 +16,19 @@ const Reservation = () => {
                     {/* restaurant card */}
                     <div id='disclaimer'>
                         <h3>Reservation Policy</h3>
-                        <p>Reservation Procedure: Our restaurant accepts reservations through our online reservation form, available on our official website. Reservations can also be made via phone during operating hours.
-Reservation Changes and Cancellations: Customers may modify or cancel their reservation by contacting the restaurant directly via phone or email. Any changes or cancellations must be made at least 24 hours before the reservation time to avoid penalties.
-Cancellation Policy: A cancellation policy is in place to manage reservation changes effectively. Customers will be subject to a cancellation fee if they fail to cancel within the specified time</p>
+                        <p>1. Reservation Procedure: Our restaurant accepts reservations through our online reservation form, available on our official website. Reservations can also be made via phone during operating hours.
+                        <br/>2. Reservation Changes and Cancellations: Customers may modify or cancel their reservation by contacting the restaurant directly via phone or email. Any changes or cancellations must be made at least 24 hours before the reservation time to avoid penalties.
+                        <br/>3. Cancellation Policy: A cancellation policy is in place to manage reservation changes effectively. Customers will be subject to a cancellation fee if they fail to cancel within the specified time</p>
                     </div>
                 </div>
                 <div id='form'>
                     <h2>RESERVATION FORM</h2>
-                    <div id=''>Date:<input id='input'></input></div>
-                    <div>Name:<input id='input'></input></div>
-                    <div>Phone No:<input id='input'></input></div>
-                    <div>Time:<input id='input'></input></div>
-                    <div>No of pax:<input id='input'></input></div>
+                    <div id='inputs'>
+                        <div>Date:<br/><input id='input' type='datetime-local'></input></div>
+                        <div>Name:<br/><input id='input'></input></div>
+                        <div>Phone No:<br/><input id='input' type='tel'></input></div>
+                        <div>No of pax:<br/><input id='input' type='number'></input></div>
+                    </div>
                 </div>
             </div>
         </div>
