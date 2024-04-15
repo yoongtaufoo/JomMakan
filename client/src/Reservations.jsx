cc868964e6f4e74fd115babf7398132591811e32
AK <adelinekgen@gmail.com>
2024-04-15 21:38:01 +0800

    Update Reservations.jsx

--- client/src/Reservations.jsx
+++ client/src/Reservations.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./Reservations.css"
 import Navbar from './components/Navbar'
+import SearchBar from "./components/SearchBar";
+import image from "./assets/image 3.png";
 
 const Reservations = () => {
   return (
     <div>
       <Navbar/>
+      <img src={image} alt="" style={{ width: '100%' }}/>
+      <div id='container'>
+        <div className="d-flex justify-content-between align-items-center">
+          <h1 className='custom-h1'>My Reservations</h1> 
+          <SearchBar place="Locations, Restaurant, or Dates..."/>
+        </div>
+      </div>
     </div>
   )
 }
