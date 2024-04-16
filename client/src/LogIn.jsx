7be36340296cde774bf4470bc99151a55487066c
Yoong <81151290+yoongtaufoo@users.noreply.github.com>
2024-04-17 06:57:35 +0800

    Update Login

--- client/src/LogIn.jsx
+++ client/src/LogIn.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./LogIn.css";
 import { Link } from "react-router-dom";
 import logo from "./assets/logo.png";
+import loginLeft from "./assets/log-in-left.jpg";
+import loginRight from "./assets/log-in-right.jpg";
 
 const LogIn = () => {
   const handleSubmit = (event) => {
@@ -16,51 +18,57 @@ const LogIn = () => {
           <button>Login</button>
         </Link> */}
       </nav>
-      <div className="log-in-container">
-        <h1>Log in to your account</h1>
-        <form onSubmit={handleSubmit} id="log-in-form">
-          <div class="form-group">
-            <label for="emailLogIn">
-              <strong>Email</strong>
-            </label>
-            <input
-              type="email"
-              class="form-control log-in"
-              id="emailLogIn"
-              // aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            {/* <small id="emailHelp" class="form-text text-muted">
+      <div class="w-100 d-flex flex-row justify-content-between align-items-center">
+        <img
+          className="log-in-pic pic-left"
+          src={loginLeft}
+          alt="left log in pic"
+        />
+        <div className="log-in-container">
+          <h1>Log in to your account</h1>
+          <form onSubmit={handleSubmit} id="log-in-form">
+            <div class="form-group">
+              <label for="emailLogIn">
+                <strong>Email</strong>
+              </label>
+              <input
+                type="email"
+                class="form-control log-in"
+                id="emailLogIn"
+                // aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              {/* <small id="emailHelp" class="form-text text-muted">
             We'll never share your email with anyone else.
           </small> */}
-          </div>
-          <div class="form-group">
-            <label for="passwordLogIn">
-              <strong>Password</strong>
-            </label>
-            <input
-              type="password"
-              class="form-control log-in"
-              id="passwordLogIn"
-              placeholder="Enter Password"
-            />
-          </div>
-          <Link to="/home">
-            <button type="submit" id="log-in-button">
-              Log In
-            </button>
-          </Link>
-          <br />
-          <p>
-            <strong>Don't have an account ?</strong>
-          </p>
-          <a
-            class="link-danger link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-            href="/signup"
-          >
-            Click here to sign up !
-          </a>
-          {/* <label htmlFor="">Email Address: </label>
+            </div>
+            <div class="form-group">
+              <label for="passwordLogIn">
+                <strong>Password</strong>
+              </label>
+              <input
+                type="password"
+                class="form-control log-in"
+                id="passwordLogIn"
+                placeholder="Enter Password"
+              />
+            </div>
+            <Link to="/home">
+              <button type="submit" id="log-in-button">
+                Log In
+              </button>
+            </Link>
+            <br />
+            <p>
+              <strong>Don't have an account ?</strong>
+            </p>
+            <a
+              class="link-danger link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+              href="/signup"
+            >
+              Click here to sign up !
+            </a>
+            {/* <label htmlFor="">Email Address: </label>
         <br></br>
         <input
           type="email"
@@ -69,8 +77,10 @@ const LogIn = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         /> */}
-          {/* <button type="submit">Log In</button> */}
-        </form>
+            {/* <button type="submit">Log In</button> */}
+          </form>
+        </div>
+        <img className="log-in-pic" src={loginRight} alt="right log in pic" />
       </div>
     </div>
   );
