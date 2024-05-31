43e368193631946c698dd5e260ff3f16955f8dd3
PeiHui369 <leepeihui.lph@gmail.com>
2024-06-01 00:01:48 +0800

    Updated WorkshopCard favWorkshop workshopController workshopRoute Main; Created favWorkshopModel

--- server/controllers/workshop.controller.js
+++ server/controllers/workshop.controller.js
@@ -1,24 +1,18 @@
-// controllers/workshopController.js
 const Workshop = require("../models/workshopModel");
+const favWorkshop = require("../models/favWorkshopModel.js")
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const workshops = async (req, res) => {
   try {
-    // Get the current date
-    const currentDate = new Date();
-
-    // Find workshops with dates greater than the current date
-    const upcomingWorkshops = await Workshop.find({ date: { $gt: currentDate } });
-
-    // Respond with the filtered workshops
-    res.json(upcomingWorkshops);
+    const workshops = await Workshop.find();
+    res.json(workshops);
   } catch (error) {
     console.error("Error fetching workshops: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
 
 const workshopDetails = async (req, res) => {
   try {
@@ -26,72 +20,142 @@ const workshopDetails = async (req, res) => {
     if (!workshop) {
       return res.status(404).json({ message: "Workshop not found" });
     }
-    res.json(workshop);
+
+    const authHeader = req.headers.authorization; //Get token
+    if (!authHeader){
+      console.log("No token provided");
+      return res.json({ workshop }); //Return workshop details without isSaved flag
+    }
+    const token = JSON.parse(authHeader)
+    console.log("Authorization Header:", token);
+
+    if (!token){
+      console.log("No token provided");
+      return res.json({ workshop });// Return workshop details without isSaved flag
+    }
+
+    const userId = token.user._id
+    const savedWorkshop = await favWorkshop.findOne({ user_id: userId, workshop_id: req.params._id });
+    const isSaved = !!savedWorkshop;
+
+    console.log("isSaved:", isSaved);
+    console.log("Workshop:", workshop);
+
+    //include the isSaved flag in the response
+    return res.json({ workshop, isSaved });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
-const addFavouriteWorkshops = async (req, res) => {
-  const { workshopId } = req.params;
-  if (!workshopId) {
-    return res.status(400).json({ message: "workshopId is missing in the request body" });
-  }
+// POST add favourite workshop
+const saveWorkshop = async (req, res) => {
+  const {
+    workshopName,
+    workshopDescription,
+    phoneNumber,
+    address,
+    time,
+    date,
+    availableSlot,
+    photoLink,
+    workshop_id,  
+  } = req.body;
 
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const userId = decoded.userId;
+    const authHeader = req.headers.authorization; // Get token
+    const token = JSON.parse(authHeader);
 
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    const userId = token.user._id; // Get the userId from the decoded token
+    console.log(userId)
 
-    if (user.favWorkshops.some(fav => fav.favWorkshopId.equals(workshopId))) {
-      return res.status(400).json({ message: "Workshop is already in favorites" });
-    }
+    // Create a new favourite restaurant to db
+    const newFavWorkshop = new favWorkshop({
+      workshopName,
+      workshopDescription,
+      phoneNumber,
+      address,
+      time,
+      date,
+      availableSlot,
+      photoLink,
+      workshop_id,
+      user_id: userId,
+    });
+    await newFavWorkshop.save();
 
-    user.favWorkshops.push({ favWorkshopId: workshopId });
-    await user.save();
-
-    res.status(201).json({ message: "Workshop added to favorites" });
+    res.status(201).json({ message: "Workshop created successfully" });
   } catch (err) {
-    console.error("Error adding workshop to favorites: ", err);
-    res.status(500).json({ message: "Failed to add workshop to favorites" });
+    console.error("Error creating workshop:", err);
+    res.status(500).json({ message: "Failed to create workshop" });
   }
 };
 
-const removeFavouriteWorkshops = async (req, res) => {
-  const { workshopId } = req.params;
-  if (!workshopId) {
-    return res.status(400).json({ message: "workshopId is missing in the request body" });
-  }
-
+// Get fav workshop
+const myFavouriteWorkshop = async (req, res) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const userId = decoded.userId;
+    const authHeader = req.headers.authorization; // Get token
+    const token = JSON.parse(authHeader);
 
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    const userId = token.user._id; // Get the userId from the decoded token
 
-    user.favWorkshops = user.favWorkshops.filter(fav => !fav.favWorkshopId.equals(workshopId));
-    await user.save();
+    const saved = await favWorkshop.find({ user_id: userId }); // Find saved workshop with user_id = userId
+    console.log(saved)
 
-    res.status(200).json({ message: "Workshop removed from favorites" });
+    res.json(saved); // Send sorted saved workshop array as response
+    
   } catch (err) {
-    console.error("Error removing workshop from favorites: ", err);
-    res.status(500).json({ message: "Failed to remove workshop from favorites" });
+    console.error("Error fetching favourite workshop:", err);
+    res.status(500).json({ message: "Failed to fetch favourite workshop" });
   }
 };
+
+
+
+// Delete the favorite workshop
+const deleteFavWorkshop = async (req, res) => {
+  try {
+
+    const deleted = await favWorkshop.findByIdAndDelete({"_id": req.params._id});
+    if (!deleted) {
+      return res.status(404).json({ message: "Favorite workshop not found" });
+    }
+
+    res.json({ message: "Favorite workshop removed successfully" }); // Send a success message as response
+  } catch (err) {
+    console.error("Error deleting favourite workshop:", err);
+    res.status(500).json({ message: "Failed to delete favourite workshop" });
+  }
+};
+
+const getFavWorkshopById = async (req, res) => {
+  try {
+
+    const authHeader = req.headers.authorization; // Get token
+    const token = JSON.parse(authHeader);
+
+    const userId = token.user._id;
+
+    const workshop = await favWorkshop.findOne({ "workshop_id": req.params._id, user_id: userId });
+    if (!workshop) {
+      return res.status(404).json({ message: "Favorite workshop not found" });
+    }
+    return res.status(200).json(workshop);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+
+};
+
 
 module.exports = {
   workshops,
   workshopDetails,
-  addFavouriteWorkshops,
-  removeFavouriteWorkshops
+  saveWorkshop,
+  myFavouriteWorkshop,
+  deleteFavWorkshop,
+  getFavWorkshopById
 };
+
