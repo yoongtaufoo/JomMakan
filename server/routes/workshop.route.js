692fe97760d6ea5dde98032ca70671e4fda6fee6
PeiHui369 <leepeihui.lph@gmail.com>
2024-06-01 17:58:12 +0800

    star functioning save unsave

--- server/routes/workshop.route.js
+++ server/routes/workshop.route.js
@@ -1,24 +1,20 @@
-//const authenticateToken = require("../authenticateToken")
 const express = require("express");
 const {
-    workshops,
-    workshopDetails,
-    saveWorkshop,
-    myFavouriteWorkshop,
-    deleteFavWorkshop,
-    getFavWorkshopById,
+  workshops,
+  workshopDetails,
+  saveWorkshop,
+  myFavouriteWorkshop,
+  deleteFavWorkshop,
+  getFavWorkshopById,
 } = require("../controllers/workshop.controller");
-
 
 const router = express.Router();
 
 router.get("/workshops", workshops);
-//router.get("/workshop/:_id", workshopDetails); 
-router.get("/:_id", workshopDetails); 
-router.get("/favworkshops", myFavouriteWorkshop );
-router.delete("/favworkshops/:_id", deleteFavWorkshop); 
+router.get("/:_id", workshopDetails);
+router.get("/favworkshops", myFavouriteWorkshop);
+router.delete("/favworkshops/:_id", deleteFavWorkshop);
 router.get("/favworkshops/:_id", getFavWorkshopById);
-router.post("/:workshopId/addFavWorkshop", saveWorkshop)
+router.post("/:workshopId/addFavWorkshop", saveWorkshop);
 
 module.exports = router;
-
